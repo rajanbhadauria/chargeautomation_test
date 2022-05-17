e08affe870267e8590b2c4b149bae7359ee90712
Rajan Bhadauria <rajanbhadauria@gmail.com>
2022-05-17 14:53:25 +0530

    Stripe Billing test base file added

---
+++ pages/ApplicationFee.py
@@ -0,0 +1,20 @@
+import stripe
+stripe.api_key = "sk_test_qyGavVyibjgXtyoWCTtDQhme"
+
+
+class ApplicationFee:
+
+    def getCustomers(self, limitcount=1):
+        customers = stripe.Customer.list(limit=limitcount)
+        for customer in customers["data"]:
+            print(customer['email'])
+
+    def getPaymentIntents(self, limitcount = 1):
+        payment_intents = stripe.PaymentIntent.list(limit=limitcount)
+        for payment_intent in payment_intents["data"]:
+            print(payment_intent)
+
+
+appFee = ApplicationFee()
+appFee.getPaymentIntents(10)
+
