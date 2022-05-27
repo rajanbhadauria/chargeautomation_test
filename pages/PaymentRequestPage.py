5d3f48034a37f72bae76378253b667c734f36991
rajanbhadauria <rajanbhadauria@gmail.com>
2022-05-27 18:37:22 +0530

    Payment Request test cases added

--- pages/PaymentRequestPage.py
+++ pages/PaymentRequestPage.py
@@ -5,6 +5,26 @@ class PaymentRequestPage:
     def __init__(self, driver):
         self.driver = driver
 
+    def paymentRequestLink(self):
+        return self.driver.find_element(By.LINK_TEXT, 'Payment Request')
+
     def managePaymentRequestLink(self):
         return self.driver.find_element(By.XPATH, '//a[contains(@href, "payment-requests")]')
 
+    def addPaymentRequestLink(self):
+        return self.driver.find_element(By.PARTIAL_LINK_TEXT, 'Add New Request')
+
+    def sendPaymentRequest(self):
+        return self.driver.find_element(By.XPATH, "//div[contains(@class, 'dropup')]/button[contains(@class, 'pl-3')]")
+
+    def emailInput(self):
+        return self.driver.find_element(By.ID, 'transactionContact')
+
+    def emailError(self):
+        return self.driver.find_element(By.XPATH, "//input[@id= 'transactionContact']/parent::div/small/strong")
+
+    def amountInput(self):
+        return self.driver.find_element(By.ID, 'transactionAmount')
+
+    def amountError(self):
+        return self.driver.find_element(By.XPATH, "//input[@id= 'transactionAmount']/parent::div/parent::div/small/strong")
--- tests/test_payment_request.py
+++ tests/test_payment_request.py
@@ -1,26 +1,82 @@
+import time
+
 import pytest
 
 from pages.HomePage import HomePage
 from pages.LoginPage import LoginPage
+from pages.PaymentRequestPage import PaymentRequestPage
 from utilities.BaseClass import BaseClass
+
 
 @pytest.mark.usefixtures('loginData')
 class TestPaymentRequest(BaseClass):
 
     # Login to site
     def test_login(self, loginData):
-        """Login to charge automation"""
+        """Test Payment request login to charge automation"""
         log = self.myLogger()
         homePage = HomePage(self.driver)
         loginPage = LoginPage(self.driver)
+        paymentRequestPage = PaymentRequestPage(self.driver)
         log.info("Clicking to login link")
         homePage.loginBtn().click()
         log.info("Filling login form")
         loginPage.emailInput().send_keys(loginData['email'])
-        log.info("Login email - "+f"'{loginPage.emailInput().get_attribute('value')}'")
+        log.info("Login email - " + f"'{loginPage.emailInput().get_attribute('value')}'")
         loginPage.passwordInput().send_keys(loginData['password'])
         log.info("Login password - " + f"'{loginPage.passwordInput().get_attribute('value')}'")
         loginPage.loginSubmitBtn().click()
+        time.sleep(2)
+        log.info("Redirecting manage payment request link")
+        paymentRequestPage.paymentRequestLink().click()
+        paymentRequestPage.managePaymentRequestLink().click()
+        time.sleep(2)
 
+    # Test create payment request with blank data
+    def test_create_payment_request_with_blank_data(self):
+        """Test create payment request with blank data"""
+        log = self.myLogger()
+        paymentRequestPage = PaymentRequestPage(self.driver)
+        paymentRequestPage.addPaymentRequestLink().click()
+        log.info("Submit form with blank data")
+        paymentRequestPage.amountInput().clear()
+        paymentRequestPage.sendPaymentRequest().click()
+        time.sleep(2)
+        log.info("Email validation message " + f"'{paymentRequestPage.emailError().text}'")
+        assert ('email field is required' in paymentRequestPage.emailError().text)
+        log.info("Email validation message " + f"'{paymentRequestPage.amountError().text}'")
+        assert ('amount must be at least 1' in paymentRequestPage.amountError().text)
 
+    # Test create payment request with invalid email and amount
+    def test_create_payment_request_with_invalid_data(self):
+        """Test create payment request with invalid email and amount"""
+        log = self.myLogger()
+        paymentRequestPage = PaymentRequestPage(self.driver)
 
+        log.info("Filling email ")
+        paymentRequestPage.emailInput().clear()
+        paymentRequestPage.emailInput().send_keys('adasdjrew%^^8942')
+        log.info("Email input data - " + f"'{paymentRequestPage.emailInput().get_attribute('value')}'")
+
+        log.info("Filling amount ")
+        paymentRequestPage.amountInput().clear()
+        paymentRequestPage.amountInput().send_keys('0cxvcx')
+        log.info("Amount input data - " + f"'{paymentRequestPage.amountInput().get_attribute('value')}'")
+
+        log.info("Submit form")
+        paymentRequestPage.sendPaymentRequest().click()
+        time.sleep(2)
+        log.info("Email validation message " + f"'{paymentRequestPage.emailError().text}'")
+        assert ('valid email address' in paymentRequestPage.emailError().text)
+        log.info("Email validation message " + f"'{paymentRequestPage.amountError().text}'")
+        assert ('The amount must be at least' in paymentRequestPage.amountError().text)
+    # Test create payment request with special characters
+    # Test create payment request with JS and PHP code
+    # Test create payment request with scheduled date
+    # Test create payment request with expiry date
+    # Test create payment request with charge back protection
+    # Test create payment request with long text in description and terms input
+    # Test create payment request with send payment link
+    # Test create payment request with create payment link
+    # Test create payment request with charge now link
+    # Test create payment request with schedule payment link
